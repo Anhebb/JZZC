abf6fc4f59d4edc8661565e9140f6cca2f526b12
LJG <2977899946@qq.com>
2019-11-29 15:19:00 +0800

    add mine and shoppingCar

--- miniprogram/pages/tabBar/shoppingCar.js
+++ miniprogram/pages/tabBar/shoppingCar.js
@@ -1,66 +1,197 @@
-// miniprogram/pages/tabBar/shoppingCar.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    dataList: [{
+      id: 1,
+      buyNum: 2
+    }, {
+      id: 2,
+      buyNum: 1
+    }],
+    actions: [{
+      name: '收藏',
+      width: 64,
+      color: '#fff',
+      fontsize: 28,
+      background: '#FFC600'
+    },
+    {
+      name: '看相似',
+      color: '#fff',
+      fontsize: 28,
+      width: 64,
+      background: '#FF7035'
+    },
+    {
+      name: '删除',
+      color: '#fff',
+      fontsize: 28,
+      width: 64,
+      background: '#F82400'
+    }
+    ],
+    actions2: [{
+      name: '看相似',
+      color: '#fff',
+      fontsize: 28,
+      width: 64,
+      background: '#FF7035'
+    },
+    {
+      name: '删除',
+      color: '#fff',
+      fontsize: 28,
+      width: 64,
+      background: '#F82400'
+    }
+    ],
+    isEdit: false,
+    productList: [{
+      img: 1,
+      name: "欧莱雅（LOREAL）奇焕光彩粉嫩透亮修颜霜 30ml（欧莱雅彩妆 BB霜 粉BB 遮瑕疵 隔离）",
+      sale: 599,
+      factory: 899,
+      payNum: 2342
+    },
+    {
+      img: 2,
+      name: "德国DMK进口牛奶  欧德堡（Oldenburger）超高温处理全脂纯牛奶1L*12盒",
+      sale: 29,
+      factory: 69,
+      payNum: 999
+    },
+    {
+      img: 3,
+      name: "【第2支1元】柔色尽情丝柔口红唇膏女士不易掉色保湿滋润防水 珊瑚红",
+      sale: 299,
+      factory: 699,
+      payNum: 666
+    },
+    {
+      img: 4,
+      name: "百雀羚套装女补水保湿护肤品",
+      sale: 1599,
+      factory: 2899,
+      payNum: 236
+    },
+    {
+      img: 5,
+      name: "百草味 肉干肉脯 休闲零食 靖江精制猪肉脯200g/袋",
+      sale: 599,
+      factory: 899,
+      payNum: 2399
+    },
+    {
+      img: 6,
+      name: "短袖睡衣女夏季薄款休闲家居服短裤套装女可爱韩版清新学生两件套 短袖粉色长颈鹿 M码75-95斤",
+      sale: 599,
+      factory: 899,
+      payNum: 2399
+    },
+    {
+      img: 1,
+      name: "欧莱雅（LOREAL）奇焕光彩粉嫩透亮修颜霜",
+      sale: 599,
+      factory: 899,
+      payNum: 2342
+    },
+    {
+      img: 2,
+      name: "德国DMK进口牛奶",
+      sale: 29,
+      factory: 69,
+      payNum: 999
+    },
+    {
+      img: 3,
+      name: "【第2支1元】柔色尽情丝柔口红唇膏女士不易掉色保湿滋润防水 珊瑚红",
+      sale: 299,
+      factory: 699,
+      payNum: 666
+    },
+    {
+      img: 4,
+      name: "百雀羚套装女补水保湿护肤品",
+      sale: 1599,
+      factory: 2899,
+      payNum: 236
+    }
+    ],
+    pageIndex: 1,
+    loadding: false,
+    pullUpOn: true
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  changeNum: function (e) {
+    console.log(e)
+    let value = `dataList[${e.detail.index}].buyNum`
+    this.setData({
+      [value]: e.detail.value
+    })
   },
+  handlerButton: function (e) {
+    console.log(e)
+    let index = e.detail.index;
+    let item = e.detail.item;
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+    wx.showToast({
+      title: `商品id：${item.id}，按钮index：${index}`,
+      icon: "none"
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  editGoods: function () {
+    this.setData({
+      isEdit: !this.data.isEdit
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  detail: function () {
+    wx.navigateTo({
+      url: '../../productDetail/productDetail'
+    })
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
+  btnPay() {
+    wx.navigateTo({
+      url: '../submitOrder/submitOrder'
+    })
+  },
+  onPullDownRefresh() {
+    setTimeout(() => {
+      wx.stopPullDownRefresh()
+    }, 200)
+  },
   onPullDownRefresh: function () {
-
+    let loadData = JSON.parse(JSON.stringify(this.data.productList));
+    loadData = loadData.splice(0, 10)
+    this.setData({
+      productList: loadData,
+      pageIndex: 1,
+      pullUpOn: true,
+      loadding: false
+    })
+    wx.stopPullDownRefresh()
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
   onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    if (!this.data.pullUpOn) return;
+    this.setData({
+      loadding: true
+    })
+    if (this.data.pageIndex == 4) {
+      this.setData({
+        loadding: false,
+        pullUpOn: false
+      })
+    } else {
+      let loadData = JSON.parse(JSON.stringify(this.data.productList));
+      loadData = loadData.splice(0, 10)
+      if (this.data.pageIndex == 1) {
+        loadData = loadData.reverse();
+      }
+      this.setData({
+        pageIndex: this.data.pageIndex + 1,
+        loadding: false,
+        productList: this.data.productList.concat(loadData)
+      })
+    }
   }
 })
